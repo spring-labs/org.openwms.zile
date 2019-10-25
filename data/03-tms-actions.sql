6caddbe6b0511e6b5e6491c559680aa62985585a
Heiko Scherrer <hscherrer@interface21.io>
2019-10-25 23:10:21 +0200

    added demo data

---
+++ data/03-tms-actions.sql
@@ -0,0 +1,3 @@
+SET SCHEMA 'tmsrtng';
+
+insert into rsrv_action (c_name,c_description,c_route_id,c_program_name,c_location_key,c_location_group_name,c_action_type, c_enabled, c_pk, c_ol, c_created, c_updated,c_pid) values ('A0001','LOCU for the whole system','99998','CP010','','ZILE','LOCU',true,'1',0,now(),null,6474547);
